43a38e21762952c0f8c1665578db77aa70d95809
Rizwan Ahamed <rizwan_ahamed@amp.com.au>
2015-10-06 14:51:14 +1100

    trying to clone to another repo

---
+++ src/index.test.js
@@ -0,0 +1,36 @@
+var starWars = require('./index');
+var expect = require('chai').expect;
+
+describe('starwars-names', function(){
+describe('all', function () {
+	
+    it('should do what...', function () {
+    	
+    
+	expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+	function isArrayOfStrings(array) {
+		return array.every(function(item){
+			return typeof item === 'string';
+		});
+		// body...
+	};
+	});
+
+	it('should contain `Rizwa1`', function () {
+		expect(starWars.all).to.include('Rizwan');
+
+	});
+});
+
+describe('random', function () {
+	it('test random', function () {
+		var randomItem = starWars.random();
+		expect(starWars.all).to.include(randomItem);
+	});
+});
+
+
+
+});
+
